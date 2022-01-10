c5b29648fbb31e40aae931adada24082d80ebf88
Joe Valookaran <joevalookaran@hotmail.com>
2022-01-10 22:52:46 +0000

    adding the tail function to library

---
+++ tail.js
@@ -0,0 +1,20 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const tail = function (tailarr) {
+  let k = tailarr.slice(1);
+  return k;
+};
+
+let j = ["Hello", "Lighthouse", "Labs"]
+const result = tail(j);
+assertEqual(result.length, 2); // ensure we get back two elements
+assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
+assertEqual(result[1], "Labs"); // ensure second element is "Labs"
+
+console.log(j);
