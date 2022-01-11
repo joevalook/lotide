84e114be9f3fe3434b7413fa308e92389794fbc2
Joe Valookaran <joevalookaran@hotmail.com>
2022-01-11 20:50:20 +0000

    made sure the function didn't change the original array

--- without.js
+++ without.js
@@ -29,19 +29,27 @@ const assertArraysEqual = function(ar1, ar2) {
 const without = function(ar1, ar2){
   let c = ar1.length
   let d = ar2.length
-  
+  let e = []
+  for (let a of ar1){
+    e.push(a)
+  }
   for (let i = 0; i < c; i++){
     for (let j = 0; j < d; j++){
-      if (ar1[i] === ar2[j]){
-        ar1.splice(i,1);
+      if (e[i] === ar2[j]){
+        e.splice(i,1);
         i -= 1
       }
     }
   }
-  return ar1
+  return e
 };
 
 console.log(without([0,1,2],[1]));
 console.log(without([0,1,"2"],[2]));
 console.log(without([0,1,1],[3]));
 console.log(without([0,1,2,2,3,1],[2,1]));
+
+const words = ["hello", "world", "lighthouse"];
+without(words, ["lighthouse"]); // no need to capture return value for this test case
+// Make sure the original array was not altered by the without function
+assertArraysEqual(words, ["hello", "world", "lighthouse"]);
