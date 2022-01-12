a00d5e275b8eac4e0102db42eacaa5dae78f8338
Joe Valookaran <joevalookaran@hotmail.com>
2022-01-12 20:54:29 +0000

    made the letter positions script

---
+++ letterPositions.js
@@ -0,0 +1,43 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const eqArrays = function (ar1, ar2) {
+  if (ar1.length !== ar2.length) {
+    return false;
+  }
+  for (let i =0; i < ar1.length; i++) {
+    if (ar1[i] !== ar2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(ar1, ar2) {
+  if (eqArrays(ar1,ar2) === true) {
+    console.log(`✅✅✅ Assertion Passed: ${ar1} === ${ar2}`);
+  } else {
+    console.log(`❌❌❌ Assertion Failed: ${ar1} !== ${ar2}`);
+  }
+};
+
+const letterPositions = function(sentence) {
+  const results = {};
+  for (i = 0; i < sentence.length; i++) {
+    if (results[sentence[i]]) {
+      
+      results[sentence[i]].push(i);
+    } else if (sentence[i] !== " ") {
+      results[sentence[i]] = [i];
+    }
+    
+  }
+  console.log(results)
+  return results;
+}
+letterPositions("sentence is there for you");
