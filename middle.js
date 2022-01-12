99f3e0218f61af97e06d7204feb503c76e7a0a26
Joe Valookaran <joevalookaran@hotmail.com>
2022-01-12 18:46:37 +0000

    added the middle file to lotide

---
+++ middle.js
@@ -0,0 +1,49 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const eqArrays = function (ar1, ar2) {
+  if (ar1.length !== ar2.length) {
+    return false;
+  }
+  for (let i =0; i < ar1.length; i++) {
+    if (ar1[i] !== ar2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(ar1, ar2) {
+  if (eqArrays(ar1,ar2) === true) {
+    console.log(`✅✅✅ Assertion Passed: ${ar1} === ${ar2}`);
+  } else {
+    console.log(`❌❌❌ Assertion Failed: ${ar1} !== ${ar2}`);
+  }
+};
+
+const middle = function(arr) {
+  if (arr.length == 1 || arr.length == 2 || arr.length == 0) {
+    return []
+  } else if (arr.length % 2 === 0) {
+    let a = []
+    a.push(arr[arr.length/2 - 1]);
+    a.push(arr[arr.length/2]);
+    return a
+  } else {
+    let a =[]
+    a.push(arr[Math.floor(arr.length/2)])
+    return a
+  }
+}
+
+console.log(middle([1])) // => []
+console.log(middle([1, 2])) // => []
+console.log(middle([1, 2, 3])) // => [2]
+console.log(middle([1, 2, 3, 4, 5])) // => [3]
+console.log(middle([1, 2, 3, 4])) // => [2, 3]
+console.log(middle([1, 2, 3, 4, 5, 6])) // => [3, 4]
