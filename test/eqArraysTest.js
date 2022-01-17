473707f531bf48565e10783b0e77f587036bd3e7
Joe Valookaran <joevalookaran@hotmail.com>
2022-01-17 19:36:55 +0000

    module exports for tail and equal arrays

---
+++ test/eqArraysTest.js
@@ -0,0 +1,9 @@
+const eqArrays = require('../eqArrays');
+const assertEqual = require('../assertEqual');
+
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+assertEqual(eqArrays([1, 2, 3], [1, 2]), false);
+assertEqual(eqArrays([1, 2, 3], [1, 2, 4]), false);
+assertEqual(eqArrays([1, 2, 3], [1, 2, "3"]), false);
+assertEqual(eqArrays([1, 2, 3], [1, 1]), false);
